c2e6c971a238351c05d0d5b0732acdfadbe04f56
Brandyce <bunja77@sbcglobal.net>
2018-11-07 20:10:39 -0800

    dayTwo done

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,64 @@
-var name= 'Ben'
-
-console.log(name)
-
-function() {
-  console.log('skdfsk');
-  console.log('skdf');
-
-  if(true) {
-  console.log('skdfsk')
-}
+// // string (primitive)
+// var name= 'Ben'
+// // string object
+// var nameTwo = new String("Ben");
+// // number
+// var luckynNumber= 42;
+// // boolean
+// var boolean= true;
+//
+// console.log(typeof(name));
+//
+// console.log(name)
+//
+// function() {
+//   console.log('skdfsk');
+//   console.log('skdf');
+//
+//   if(true) {
+//   console.log('skdfsk')
+// }
+//
+// var firstName = "Brandyce";
+//
+// console.log("Brandyce", firstName);
+// // new
+// var name = "Ben";
+//
+// {
+//   var name = "Norma";
+//   console.log(name);
+// }
+// console.log(name);
+//
+// // new
+// const date = new Date('09/37/89');
+//
+// console.log(date);
+//
+// // nameTwo
+// const str = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
+// console.log(str.length);
+//
+// const num = new Number(42);
+// const num = 42;
+//
+// const array = [1, 2, 3];
+// const new array = new Array(1, 2, 3);
+// const arrayMixed = ['Ben', 29, true];
+// console.log(array2.length);
+//
+// const obj = {
+//   name: 'Ben',
+//   age: 29
+// }
+//
+// alert('Danger Will Robinson');
+// const response = confirm('Are you sure?');
+// console.log(response);
+//
+// const favNum = prompt('What is your favorite number?')
+// const realNum = parsInt(favNum);
+// const num = 55;
+//
+// console.log(num.toString(num));
